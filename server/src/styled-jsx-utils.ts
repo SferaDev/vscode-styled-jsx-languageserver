ef274cacebd8686f2ea325311fe36c4c142c1bfb
Southclaws <hello@southcla.ws>
2020-02-28 13:42:46 +0000

    chore: clean up repo
    - Readme update, with maintainer change note
    - Add a license, MIT because it was specified in the package.json though no actual license file was present
    - Update package.json with new publisher information
    - Quick prettier.js run over source files (apologies for git blame!)

--- server/src/styled-jsx-utils.ts
+++ server/src/styled-jsx-utils.ts
@@ -1,192 +1,226 @@
-import * as ts from 'typescript';
-import { TextDocument } from 'vscode-languageserver-types';
-import { Stylesheet } from 'vscode-css-languageservice';
-import { LanguageModelCache } from './language-model-cache';
+import * as ts from "typescript";
+import { TextDocument } from "vscode-languageserver-types";
+import { Stylesheet } from "vscode-css-languageservice";
+import { LanguageModelCache } from "./language-model-cache";
 
 export interface StyledJsxTaggedTemplate {
-    start: number;
-    end: number;
+  start: number;
+  end: number;
 }
 
 export interface StyledJsx {
-    cssDocument: TextDocument;
-    stylesheet: Stylesheet;
+  cssDocument: TextDocument;
+  stylesheet: Stylesheet;
 }
 
 const styledJsxPattern = /((<\s*?style\s*?(global)?\s*?jsx\s*?(global)?\s*?>)|(\s*?css\s*?`)|(\s*?css\.global\s*?`))/g;
-export function getApproximateStyledJsxOffsets(document: TextDocument): number[] {
-    const results = [];
-    const doc = document.getText();
-    while (styledJsxPattern.exec(doc)) {
-        results.push(styledJsxPattern.lastIndex);
-    }
-    return results;
+export function getApproximateStyledJsxOffsets(
+  document: TextDocument
+): number[] {
+  const results = [];
+  const doc = document.getText();
+  while (styledJsxPattern.exec(doc)) {
+    results.push(styledJsxPattern.lastIndex);
+  }
+  return results;
 }
 
 function getTemplateString(node: ts.Node) {
-    if (ts.isTemplateHead(node) || ts.isTemplateLiteral(node)) {
-        if (ts.isTemplateHead(node)) {
-            return node.parent;
-        } else {
-            return node;
-        }
+  if (ts.isTemplateHead(node) || ts.isTemplateLiteral(node)) {
+    if (ts.isTemplateHead(node)) {
+      return node.parent;
+    } else {
+      return node;
     }
+  }
 
-    return undefined;
+  return undefined;
 }
 
 // css`button { position: relative; }`
-export function isStyledJsxTaggedTemplate(templateNode: ts.TemplateExpression | ts.TemplateLiteral): boolean {
-    const parent = templateNode.parent;
+export function isStyledJsxTaggedTemplate(
+  templateNode: ts.TemplateExpression | ts.TemplateLiteral
+): boolean {
+  const parent = templateNode.parent;
 
-    if (ts.isTaggedTemplateExpression(parent)) {
-        if (parent.tag.getText() === 'css') {
-            return true;
-        }
+  if (ts.isTaggedTemplateExpression(parent)) {
+    if (parent.tag.getText() === "css") {
+      return true;
     }
+  }
 
-    return false;
+  return false;
 }
 
 function walk(node: ts.Node, callback: (node: ts.Node) => void) {
-    if (
-        ts.isJSDoc(node) ||
-        node.kind === ts.SyntaxKind.MultiLineCommentTrivia ||
-        node.kind === ts.SyntaxKind.SingleLineCommentTrivia
-    ) {
-        return;
-    }
+  if (
+    ts.isJSDoc(node) ||
+    node.kind === ts.SyntaxKind.MultiLineCommentTrivia ||
+    node.kind === ts.SyntaxKind.SingleLineCommentTrivia
+  ) {
+    return;
+  }
 
-    if (ts.isToken(node) && node.kind !== ts.SyntaxKind.EndOfFileToken) {
-        callback(node);
-    } else {
-        node.getChildren().forEach(child => walk(child, callback));
-    }
+  if (ts.isToken(node) && node.kind !== ts.SyntaxKind.EndOfFileToken) {
+    callback(node);
+  } else {
+    node.getChildren().forEach((child) => walk(child, callback));
+  }
 }
 
 function isStyledJsxTemplate(node: ts.Node) {
-    if (!ts.isJsxExpression(node.parent)) {
-        return false;
-    }
-
-    const grandparent = node.parent.parent;
-
-    if (!ts.isJsxElement(grandparent)) {
-        return false;
-    }
-
-    const opener = grandparent.openingElement;
-
-    if (opener.tagName.getText() !== 'style') {
-        return false;
-    }
-
-    for (const prop of opener.attributes.properties) {
-        if (prop.name && prop.name.getText() === 'jsx') {
-            return true;
-        }
-    }
-
+  if (!ts.isJsxExpression(node.parent)) {
     return false;
+  }
+
+  const grandparent = node.parent.parent;
+
+  if (!ts.isJsxElement(grandparent)) {
+    return false;
+  }
+
+  const opener = grandparent.openingElement;
+
+  if (opener.tagName.getText() !== "style") {
+    return false;
+  }
+
+  for (const prop of opener.attributes.properties) {
+    if (prop.name && prop.name.getText() === "jsx") {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 export function findStyledJsxTaggedTemplate(
-    textDocument: TextDocument,
-    cursorOffsets: number[]
+  textDocument: TextDocument,
+  cursorOffsets: number[]
 ): StyledJsxTaggedTemplate[] {
-    const source = ts.createSourceFile(
-        'tmp',
-        textDocument.getText(),
-        ts.ScriptTarget.Latest,
-        true,
-        ts.ScriptKind.JSX | ts.ScriptKind.TSX
-    );
+  const source = ts.createSourceFile(
+    "tmp",
+    textDocument.getText(),
+    ts.ScriptTarget.Latest,
+    true,
+    ts.ScriptKind.JSX | ts.ScriptKind.TSX
+  );
 
-    const templates: StyledJsxTaggedTemplate[] = [];
+  const templates: StyledJsxTaggedTemplate[] = [];
 
-    walk(source, node => {
-        const templateNode = getTemplateString(node);
+  walk(source, (node) => {
+    const templateNode = getTemplateString(node);
 
-        if (templateNode) {
-            if (isStyledJsxTaggedTemplate(templateNode) || isStyledJsxTemplate(templateNode)) {
-                templates.push({ start: templateNode.getStart() + 1, end: templateNode.getEnd() - 1 });
-            }
-        }
-    });
+    if (templateNode) {
+      if (
+        isStyledJsxTaggedTemplate(templateNode) ||
+        isStyledJsxTemplate(templateNode)
+      ) {
+        templates.push({
+          start: templateNode.getStart() + 1,
+          end: templateNode.getEnd() - 1
+        });
+      }
+    }
+  });
 
-    return templates;
+  return templates;
 }
 
 const expressionPattern = /(\${.*})|(&&|\|\|)/g;
 // I guess so long functions are bad. Don't know how to properly format in typescript.
 export function replaceAllWithSpacesExceptCss(
-    textDocument: TextDocument,
-    styledJsxTaggedTemplates: StyledJsxTaggedTemplate[],
-    stylesheets: LanguageModelCache<Stylesheet>
+  textDocument: TextDocument,
+  styledJsxTaggedTemplates: StyledJsxTaggedTemplate[],
+  stylesheets: LanguageModelCache<Stylesheet>
 ): { cssDocument: TextDocument; stylesheet: Stylesheet } {
-    const text = textDocument.getText();
-    let result = '';
-    // code that goes before CSS
-    result += text.slice(0, styledJsxTaggedTemplates[0].start).replace(/./g, ' ');
+  const text = textDocument.getText();
+  let result = "";
+  // code that goes before CSS
+  result += text.slice(0, styledJsxTaggedTemplates[0].start).replace(/./g, " ");
 
-    for (let i = 0; i < styledJsxTaggedTemplates.length; i++) {
-        // CSS itself with dirty hacks. Maybe there is better solution.
-        // We need to find all expressions in css and replace each character of expression with space.
-        // This is neccessary to preserve character count
-        result += text
-            .slice(styledJsxTaggedTemplates[i].start, styledJsxTaggedTemplates[i].end)
-            .replace(expressionPattern, (str, p1) => {
-                return p1.replace(/./g, ' ');
-            });
-        // if there is several CSS parts
-        if (i + 1 < styledJsxTaggedTemplates.length) {
-            // code that is in between that CSS parts
-            result += text
-                .slice(styledJsxTaggedTemplates[i].end, styledJsxTaggedTemplates[i + 1].start)
-                .replace(/./g, ' ');
-        }
-    }
-    // code that goes after CSS
+  for (let i = 0; i < styledJsxTaggedTemplates.length; i++) {
+    // CSS itself with dirty hacks. Maybe there is better solution.
+    // We need to find all expressions in css and replace each character of expression with space.
+    // This is neccessary to preserve character count
     result += text
-        .slice(styledJsxTaggedTemplates[styledJsxTaggedTemplates.length - 1].end, text.length)
-        .replace(/./g, ' ');
-
-    const cssDocument = TextDocument.create(textDocument.uri.toString(), 'css', textDocument.version, result);
-    const stylesheet = stylesheets.get(cssDocument);
-
-    return {
-        cssDocument,
-        stylesheet
+      .slice(styledJsxTaggedTemplates[i].start, styledJsxTaggedTemplates[i].end)
+      .replace(expressionPattern, (str, p1) => {
+        return p1.replace(/./g, " ");
+      });
+    // if there is several CSS parts
+    if (i + 1 < styledJsxTaggedTemplates.length) {
+      // code that is in between that CSS parts
+      result += text
+        .slice(
+          styledJsxTaggedTemplates[i].end,
+          styledJsxTaggedTemplates[i + 1].start
+        )
+        .replace(/./g, " ");
     }
+  }
+  // code that goes after CSS
+  result += text
+    .slice(
+      styledJsxTaggedTemplates[styledJsxTaggedTemplates.length - 1].end,
+      text.length
+    )
+    .replace(/./g, " ");
+
+  const cssDocument = TextDocument.create(
+    textDocument.uri.toString(),
+    "css",
+    textDocument.version,
+    result
+  );
+  const stylesheet = stylesheets.get(cssDocument);
+
+  return {
+    cssDocument,
+    stylesheet
+  };
 }
 
 export function getStyledJsx(
-    document: TextDocument,
-    stylesheets: LanguageModelCache<Stylesheet>
+  document: TextDocument,
+  stylesheets: LanguageModelCache<Stylesheet>
 ): StyledJsx | undefined {
-    const styledJsxOffsets = getApproximateStyledJsxOffsets(document);
-    if (styledJsxOffsets.length > 0) {
-        const styledJsxTaggedTemplates = findStyledJsxTaggedTemplate(document, styledJsxOffsets);
-        if (styledJsxTaggedTemplates.length > 0) {
-            return replaceAllWithSpacesExceptCss(document, styledJsxTaggedTemplates, stylesheets);
-        }
+  const styledJsxOffsets = getApproximateStyledJsxOffsets(document);
+  if (styledJsxOffsets.length > 0) {
+    const styledJsxTaggedTemplates = findStyledJsxTaggedTemplate(
+      document,
+      styledJsxOffsets
+    );
+    if (styledJsxTaggedTemplates.length > 0) {
+      return replaceAllWithSpacesExceptCss(
+        document,
+        styledJsxTaggedTemplates,
+        stylesheets
+      );
     }
-    return undefined;
+  }
+  return undefined;
 }
 
 export function getStyledJsxUnderCursor(
-    document: TextDocument,
-    stylesheets: LanguageModelCache<Stylesheet>,
-    cursorOffset: number
+  document: TextDocument,
+  stylesheets: LanguageModelCache<Stylesheet>,
+  cursorOffset: number
 ): StyledJsx | undefined {
-    const styledJsxTaggedTemplates = findStyledJsxTaggedTemplate(document, [cursorOffset]);
+  const styledJsxTaggedTemplates = findStyledJsxTaggedTemplate(document, [
+    cursorOffset
+  ]);
 
-    if (
-        styledJsxTaggedTemplates.length > 0 &&
-        styledJsxTaggedTemplates[0].start < cursorOffset &&
-        styledJsxTaggedTemplates[0].end > cursorOffset
-    ) {
-        return replaceAllWithSpacesExceptCss(document, styledJsxTaggedTemplates, stylesheets);
-    }
-    return undefined;
+  if (
+    styledJsxTaggedTemplates.length > 0 &&
+    styledJsxTaggedTemplates[0].start < cursorOffset &&
+    styledJsxTaggedTemplates[0].end > cursorOffset
+  ) {
+    return replaceAllWithSpacesExceptCss(
+      document,
+      styledJsxTaggedTemplates,
+      stylesheets
+    );
+  }
+  return undefined;
 }
